b5ce766cfbeadf495703ec28e9f95a6954065b3f
Nathan Rowbottom <rowbottomn@hdsb.ca>
2019-02-26 08:53:07 -0500

    Update HealthBar.java
    Just tidying up the health bar code to make it easier for students to implement.

--- TowerDefence2_7/HealthBar.java
+++ TowerDefence2_7/HealthBar.java
@@ -1,19 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-import java.io.*;
+import java.io.*;//for the colors instead of java.AWT.*
 /**
- * Write a description of class HealthBar here.
+ * A pretty cool health bar with a low computational footprint, and autoupdating
+ * spawn it in and then forget it
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Nathan Rowbottom
+ * @version Feb 26, 2019
  */
 public class HealthBar extends Actor
 {
-    GreenfootImage temp;
-    Color color; 
-    Enemy owner;
-    int maxHealth;
-    int size;
+    protected GreenfootImage temp;
+    protected Color color; 
+    protected Enemy owner;
+    protected int maxHealth;
+    protected int size;
+    protected int health
+    protected int red = 0, green = 0;
     
     public HealthBar(Enemy o, int maxHeath){
       //healthFill = new GreenfootImage(20,5);  
@@ -24,34 +27,50 @@ public class HealthBar extends Actor
       
     }
     
+    public void act(){
+        //check to see if our owner is gone
+        if (owner == null){
+            kys();
+            return;
+        }
+        //check to see if the object is no longer in the world 
+        //should not have to do this if the world specified the act order reasonably 
+        //but...
+        if (owner.getWorld() == null){
+            kys();
+        }
+    }
+    
+    private void kys(){
+        getWorld().removeObject(this);
+    }
     
     public void update() 
     {
+       //first update then set up some stuff
         if (temp == null){
             temp = getImage();
             temp.clear();
         }
-        //temp.clear();
-        int health = owner.health;
+      //update health   
+       health = owner.health;
+      //move the bar to the right spot
       setLocation(owner.x, owner.y-size);
-        //update the image
+      //if we are full health or dead then no update required
       if (health == maxHealth|| health <= 0){return;}
-      int red = 0, green = 0;
-      try{
+      //update the color
+      try{//not really needed anymore
           red = (int)(120+134*(1. - ((double)health/(double)maxHealth)));
           green = (int)( 254 *((double)health/(double)maxHealth));
-      color = new Color(red,green, 0);
+       color = new Color(red,green, 0);
     }
     catch(Exception e){
+        //let me know if we divided by zero somehow
        System.out.println(""+red+" , "+green);
     }
       temp.setColor(color); 
       temp.fill();
       temp.scale(1+50*health/maxHealth, 8);
-      //setImage(temp);
-      //update the position
-      //setLocation(owner.x+temp.getWidth()/2, owner.y-size);
-      
-      // Add your action code here.
+
     }    
 }
